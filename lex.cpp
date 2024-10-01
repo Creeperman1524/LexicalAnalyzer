d5e599225c07dc8fcc5f7118021b1875f4b96716
Anthony M <38088724+Creeperman1524@users.noreply.github.com>
2024-10-01 02:00:15 -0400

    Initial project setup
    Downloaded all necessary files and setup the project to be completed

---
+++ lex.cpp
@@ -0,0 +1,12 @@
+// =======================================================
+// Name        : lex.cpp
+// Description : Takes in text to be converted to tokens
+// =======================================================
+
+#include "lex.h"
+
+ostream &operator<<(ostream &out, const LexItem &tok) {}
+
+LexItem id_or_kw(const string &lexeme, int linenum) {}
+
+LexItem getNextToken(istream &in, int &linenum) {}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+// =============================================================================
+// Name        : main.cpp
+// Description : Tests the lexer by passing it the correct files and arguments
+// =============================================================================
+
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}
